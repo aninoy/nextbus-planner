4de0cf334ce43bd999e2cf81e5e04250045fb58c
Aninoy Mahapatra <aninoymahapatra@admins-MacBook-Pro.local>
2014-11-06 03:21:01 -0800

    First attempt with Nextbus API completed.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,12 +5,47 @@ angular.module('NextbusPlanner.controllers', [])
 	$scope.favs = Favs.all();
 })
 
-.controller('FavDetailCtrl', function($scope, $stateParams, Favs) {
+.controller('FavDetailCtrl', function($scope, $http, $stateParams, Favs) {
 	$scope.fav = Favs.get($stateParams.favId);
+	$http.get('http://webservices.nextbus.com/service/publicXMLFeed?command=predictionsForMultiStops&a=sf-muni&stops=N|6997&stops=N|3909');
+
 })
 
-.controller('AddCtrl', function($scope) {
-	$scope.item = 'Item Search';
+.controller('AddCtrl', function($scope, $http) {
+	$scope.fav = {
+		name: '',
+		zipCode: '',
+		state: ''
+	};
+
+	// $scope.agencies = [];
+
+	$scope.getState = function () {
+		$http.get('http://zip.getziptastic.com/v2/US/'+$scope.fav.zipCode)
+			.success(function (data) {
+				console.log(data);
+				$scope.state = data.state;
+				$scope.getAgencies(data.state);
+			}).error(function (data) {
+				console.log(data);
+			});
+	};
+
+	$scope.getAgencies = function (state) {
+		$http.get('http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList')
+			.success(function (data) {
+				console.log(state);
+				console.log('Converting to JSON...');
+				var jsonData = xmlToJSON.parseString(data);
+				console.log(jsonData.body[0].agency[0]);
+				$scope.agencies = jsonData.body[0].agency;
+
+			}).error(function (data) {
+				console.log(data);
+			});
+	};
+
+
 })
 
 .controller('SearchCtrl', function($scope) {
